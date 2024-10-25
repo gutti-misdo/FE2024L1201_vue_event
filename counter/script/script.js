d0abc8f65fa21052c65e93ace6412a5ef3766e07
gutti-misdo <2301162@s.asojuku.ac.jp>
2024-10-25 10:18:13 +0900

    crea

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,6 +3,27 @@ new Vue({
     el: '#app',
     data() {
         return {
+            count: 1,
+            input: ''
         };
+    },
+    methods: {
+        nibai() {
+            this.count = this.count*2;
+        },
+        nibunoiti() {
+            this.count = this.count/2;
+        },
+        reset() {
+            this.count = 1;
+        },
+        increment() {
+            this.count = this.count + this.input;
+        },
+        decrement() {
+            this.count = this.count - this.input;
+        }
+
+        
     }
 });
